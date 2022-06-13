ecb0782526a8f9544b10ac7e92f6663e4bdf9d8c
Enrico Egen Selian <Enrico-Egen.Selian@external.telekom.de>
2022-06-14 01:37:28 +0200

    setup react, react-router, tailwind

---
+++ Frontend/reveal.me/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1 className=" font-bold underline">Hello world!</h1>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
