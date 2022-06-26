8d95f1b9baa3cc74022416bb1f2d0dccc0c6b596
Dwiresti Puspita Rahmi <dwiresti.p.rahmi@stud.h-da.de>
2022-06-26 15:45:31 +0200

    chat implementation

--- Frontend/reveal.me/src/App.jsx
+++ Frontend/reveal.me/src/App.jsx
@@ -4,6 +4,7 @@ import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import RegisterPage from "./pages/RegisterPage";
 import Homepage from "./pages/Homepage";
 import CreateProfilePage from "./pages/CreateProfilePage";
+import ChatPage from "./pages/ChatPage";
 const App = () => {
   return (
     <BrowserRouter>
@@ -12,9 +13,8 @@ const App = () => {
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/login/forgot_password" element={<ForgotPasswordPage />}></Route>
         <Route path="/register" element={<RegisterPage />}></Route>
-        <Route path="/create_profile" element={<CreateProfilePage />}>
-
-        </Route>
+        <Route path="/create_profile" element={<CreateProfilePage />}></Route>
+        <Route path="/chat" element={<ChatPage />}></Route>
       </Routes>
       <div className="App">
         {/* <h1 className=" font-bold underline">Hello world!</h1> */}
---
+++ Frontend/reveal.me/src/pages/ChatPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from "../Components/Header";
+import ProfileFields from "../Components/ProfileFields";
+import Sidebar from "../Components/Sidebar";
+import Chat from "../Components/Chat"
+import ChatContainer from '../Components/ChatContainer';
+const ChatPage = () => {
+  return (
+    <div className='overflow-y-none'>
+        <div className="flex flex-col h-full overflow-y-none bg-pink">
+        <div>
+          <Header></Header>
+        </div>
+
+        <div className="flex flex-1 h-4/5">
+          <aside className="hidden sm:block">
+            <Sidebar></Sidebar>
+          </aside>
+          <div class="px-4 w-full h-4/5">
+            <ChatContainer></ChatContainer>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ChatPage
