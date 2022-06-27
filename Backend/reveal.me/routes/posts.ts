b65a209ea55c53fc12af2cc78c1b94f2065d845e
Starmulj <Gotujato-46>
2022-06-28 00:25:56 +0200

    -implementation get Api getAllFilteredUserById -implementation put Api updateMatchedUserById

--- Backend/reveal.me/routes/posts.ts
+++ Backend/reveal.me/routes/posts.ts
@@ -12,7 +12,8 @@ import {
   getOneUserDetailwithId,
   getOneUserDetail,
   getAllFilteredUser,
-  getAllFilteredUserById
+  getAllFilteredUserById,
+  updateMatchedUserById
 
 } from "../controllers/posts";
 
@@ -24,12 +25,13 @@ router.post("/auth/login/forgetpassword", forgetpassword);
 
 router.put("/user/profile/head/:email", updateOneUser);
 router.put("/user/profile/body/:email", updateOneUserProfile);
-router.put("/user/profile/:email/:matchedUserEmail", updateMatchedUser);
+router.put("/user/profile/:email/:matchedUserEmail", updateMatchedUser); //not use
+router.put("/user/profile/id/:id/:matchedUserId", updateMatchedUserById);
 
 router.get("/test/alluser", getAllUser);
 router.get("/test/singleuser/id/:id", getOneUserDetailwithId);
 router.get("/test/singleuser/email/:email", getOneUserDetail);
 router.get("/test/filtereduser/id/:id", getAllFilteredUserById);
-router.get("/test/filtereduser/email/:email", getAllFilteredUser);
+router.get("/test/filtereduser/email/:email", getAllFilteredUser); //not use
 
 export default router;
