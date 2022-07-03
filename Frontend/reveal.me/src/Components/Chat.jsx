afa86f1bb9eb0c89e1d3f96c7dd295b7b302825e
Dwiresti Puspita Rahmi <dwiresti.p.rahmi@stud.h-da.de>
2022-07-03 03:17:03 +0200

    [Frontend] chat list loads every 3 seconds, send message works, profile on the right side of chat is implemented. PROBLEM: only works on firefox. sometimes works sometimes doesn't. still don't know why

--- Frontend/reveal.me/src/Components/Chat.jsx
+++ Frontend/reveal.me/src/Components/Chat.jsx
@@ -3,21 +3,21 @@ import {Cookies, useCookies} from "react-cookie";
 import { useParams } from 'react-router-dom';
 import axios from "axios";
 const Chat = (props) => {
-
    const {
       matchId,
    } = props
    const [cookies, setCookie, removeCookie] = useCookies(null);
    const token = cookies.Token;
-   const { _id } = useParams();
    const id = cookies.UserId;
    const [accountData, setAccountData] = useState([])
    const [userData, setUserData] = useState([])
-   const [usersMessages2, setUsersMessages] = useState(null)
+   const [oneUserMessages, setUsersMessages] = useState(null)
    const [messages, setMessages] = useState()
-   const [mId, setMId] = useState("")
-   let userPic
-  let matchPic
+   const [loadingMatch, setLoadingMatch] = useState(false)
+   const [loadingUser, setLoadingUser] = useState(false)
+   const [textArea, setTextArea] = useState('')
+   const [sent, setSent] = useState(false)
+
    const getMatchAccount = async () => {
     try {
       const response = await axios.get(
@@ -29,10 +29,13 @@ const Chat = (props) => {
           },
         }
       );
-      const data = response.data;
-      setAccountData(data);
-      console.log(accountData)
-      matchPic = accountData.userDetail.profile_picture
+      const dataMatch = response.data;
+      setAccountData(dataMatch);
+      if(accountData.length>0 && accountData.userDetail){
+        setLoadingMatch(false)
+      }
+      else
+        setLoadingMatch(true)
 
     } catch (err) {
       console.error(err.message);
@@ -40,7 +43,7 @@ const Chat = (props) => {
   };
 
   const getUserAccount = async () => {
-      const response2 = await axios.get(
+      const response = await axios.get(
          `http://localhost:5000/api/test/singleuser/id/${id}`,
         {
           headers: {
@@ -49,10 +52,13 @@ const Chat = (props) => {
           },
         }
       )
-      const data2 = response2.data;
-      setUserData(data2);
-      console.log(userData)
-      userPic = userData.userDetail.profile_picture
+      const dataUser = response.data;
+      setUserData(dataUser);
+      if(userData.length>0 && userData.userDetail){
+         setLoadingUser(false)
+      }
+      else
+        setLoadingUser(true)
   }
 
   const getUserMessages = async ()=> {
@@ -67,74 +73,94 @@ const Chat = (props) => {
          }
       );
       const data = response.data
-      console.log(data)
       setUsersMessages(data)
-      console.log(usersMessages2.messages)
-      const newMessage = usersMessages2.messages
+      const newMessage = oneUserMessages.messages
       setMessages(newMessage)
-      console.log(messages)
    } catch (error) {
       
    }
   }
 
+   const sendMessage = async ()=>{
+      try {
+         await axios
+        .put(
+          `http://localhost:5000/api/conversation/message/${oneUserMessages._id}`,
+          {
+            userId: id,
+            message: textArea
+          },
+          {
+            headers: {
+              "Content-Type": "application/json; charset=UTF-8",
+              Authorization: `Bearer ${cookies.Token}`,
+            },
+          }
+        )
+        setSent(true);
+        setTextArea('')
+      } catch (error) {
+         console.log(error);
+      }
+   }
+
    useEffect(()=>{
     getMatchAccount()
     getUserAccount()
     getUserMessages()
-  }, [props, messages])
+  }, [matchId, messages, loadingMatch, loadingUser, sent])
 
-   const [textArea, setTextArea] = useState()
+   
   return (
-
-    <div class="flex-1 p:2 sm:p-4 justify-between flex flex-col h-4/5">
+<>
+   <div className="flex-1 p:2 sm:p-4 justify-between flex flex-col h-4/5">
    {accountData && accountData.userDetail && <div class="flex sm:items-center justify-between pb-2 border-b-2 border-gray-200">
-      <div class="relative flex items-center space-x-4">
-         <div class="relative">
-            <span class="absolute text-green-500 right-0 bottom-0">
+      <div className="relative flex items-center space-x-4">
+         <div className="relative">
+            <span className="absolute text-green-500 right-0 bottom-0">
                <svg width="20" height="20">
                   <circle cx="8" cy="8" r="8" fill="currentColor"></circle>
                </svg>
             </span>
          <img src={accountData.userDetail.profile_picture} alt="" class="w-10 sm:w-10 h-10 sm:h-10 rounded-full"/>
          </div>
-         <div class="flex flex-col leading-tight">
-            <div class="text-lg mt-1 flex items-center">
-               <span class="text-gray-700 mr-3">{`${accountData.first_name} ${accountData.last_name}`}</span>
+         <div className="flex flex-col leading-tight">
+            <div className="text-lg mt-1 flex items-center">
+               <span className="text-gray-700 mr-3">{`${accountData.first_name} ${accountData.last_name}`}</span>
             </div>
-            <span class="text-md text-gray-600">{`${accountData.userDetail.occupation}`}</span>
+            <span className="text-md text-gray-600">{`${accountData.userDetail.occupation}`}</span>
          </div>
       </div>
    </div>}
-   <div id="messages" class="flex flex-col space-y-3 p-4 overflow-y-auto scrollbar-thumb-blue scrollbar-w-2 scrolling-touch h-[648px] max-h-[1200px]">
-      {messages && messages.map((msg) =>{
+   <div id="messages" className="flex flex-col space-y-3 p-4 overflow-y-auto scrollbar-thumb-blue scrollbar-w-2 scrolling-touch h-[648px] max-h-[1200px]">
+      {messages && messages.map((msg, index) =>{
          
-         return <div key={msg.timestamp}class="chat-message">
+         return <div key={index}class="chat-message">
             
-               <div class={`flex items-end ${msg.sender === id ? ' ' : 'justify-end'}` }>
-            <div class={`flex flex-col space-y-2 text-xs max-w-xs mx-2 ${msg.sender === id ? 'order-2 item-start' : 'order-1 item-end'}`}>
-               <div><span class={`px-4 py-2 rounded-lg inline-block 
-               ${msg.sender === id ? 'rounded-bl-none bg-gray-300 text-gray-600' : 'rounded-br-none bg-pink-100 text-white'}`}>{msg.message}</span>
+               <div className={`flex items-end ${msg.sender === id ? 'justify-end' : ' '}` }>
+            <div className={`flex flex-col space-y-2 text-xs max-w-xs mx-2 ${msg.sender === id ? 'order-1 item-end' : 'order-2 item-start'}`}>
+               <div><span class={` 
+               ${msg.sender === id ? 'px-4 py-2 rounded-lg inline-block rounded-br-none bg-gray-300 text-gray-600' : 'px-4 py-2 rounded-lg inline-block rounded-bl-none bg-pink-100 text-white'}`}>{msg.message}</span>
             </div>
             </div>
-            <img src={`${msg.sender === id}` ? `${userData.userDetail.profile_picture}` : `${matchPic}`} alt="My profile" class={`w-6 h-6 rounded-full ${msg.sender === id ? "order-1" : "order-2"}`}/>
+            <img src={`${msg.sender === id ? userData.userDetail.profile_picture : accountData.userDetail.profile_picture} `} alt="My profile" className={`w-6 h-6 rounded-full ${msg.sender === id ? "order-2" : "order-1"}`}/>
          </div>
       </div>
       })}
    </div>
-   <div class="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
-      <div class="relative flex">
-         <input type="text" value={textArea} placeholder="Write your message!" class="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-3 bg-gray-200 rounded-md py-3" onChange={(e)=>setTextArea(e.target.value)}/>
-         <div class="absolute right-0 items-center inset-y-0 hidden sm:flex">
-            <button type="button" class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
-               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6 text-gray-600">
+   <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+      <div className="relative flex">
+         <input type="text" value={textArea} placeholder="Write your message!" className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-3 bg-gray-200 rounded-md py-3" onChange={(e)=>setTextArea(e.target.value)}/>
+         <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
+            <button type="button" className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
+               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-600">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path>
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path>
                </svg>
             </button>
-            <button type="button" class="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
-               <span class="font-bold">Send</span>
-               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-6 w-6 ml-2 transform rotate-90">
+            <button onClick={()=>sendMessage()} type="button" className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
+               <span className="font-bold">Send</span>
+               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90">
                   <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
                </svg>
             </button>
@@ -142,6 +168,8 @@ const Chat = (props) => {
       </div>
    </div>
 </div>
+
+</>
   )
 }
 
--- Frontend/reveal.me/src/Components/ChatContainer.jsx
+++ Frontend/reveal.me/src/Components/ChatContainer.jsx
@@ -3,28 +3,26 @@ import Chat from "./Chat";
 import axios from "axios";
 import {Cookies, useCookies} from "react-cookie";
 import useFetch from "./useFetch"
-// const {data: conversations, error, isPending} = useFetch(`http://localhost:5000/api/allconversation/${id}`, token)
-// import useFetch from "./useFetch"
+import ChatProfile from './ChatProfile';
 const ChatContainer = () => {
   const [ clickedUser, setClickedUser ] = useState(null)
-  const [matchId, setIdMatch] = useState()
+  const [matchId, setIdMatch] = useState(null)
+  const[allConversation, setConversation] = useState([]);
+  const [listOfContactDetails, setListOfContactDetail] = useState([]);
+  const [loading, setLoading] = useState(false)
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const [accountData, setAccountData] = useState([])
+  const [loadingMatch, setLoadingMatch] = useState(false)
+  const id = cookies.UserId;
+  const token = cookies.Token;
 
-    const[allConversation, setConversation] = useState([]);
-    const [senderDetail, setSenderDetail] = useState([]);
-    const [listOfContactDetails, setListOfContactDetail] = useState([]);
-    // const [usersMessages, setUsersMessages] = useState(null)
-    // const list = []
-const [cookies, setCookie, removeCookie] = useCookies(null);
-const id = cookies.UserId;
-const token = cookies.Token;
-// const {data: conversations, error, isPending} = useFetch(`http://localhost:5000/api/allconversation/${id}`, token)
+  const setChat = (_id) =>{
+    setClickedUser(true);
+    setIdMatch(_id)
+    getMatchAccount(_id)
+  }
     
-    const setChat = (_id) =>{
-      setClickedUser(true);
-      setIdMatch(_id)
-    }
-    
-    const getUserConversation = async () => {
+  const getUserConversation = async () => {
     try {
       
       const response = await fetch(`http://localhost:5000/api/allconversation/${id}`,{
@@ -34,53 +32,29 @@ const token = cookies.Token;
         },
       });
       const data = await response.json();
-      // console.log(response.data)
-      // setConversation((allConversation) => [...allConversation, ...response.data]);
       setConversation(data)
-      console.log(allConversation)
+      if(allConversation.length >0)
+      setLoading(false)
+      else
+      setLoading(true)
     } catch (error) {
-      console.log(error)
+        console.log(error)
     }
   }
-
-  //   const getUserMessages = async (_id)=> {
-  //  try {
-  //     const response = await axios.get(
-  //        `http://localhost:5000/api/oneconversation/${id}/${_id}`,
-  //        {
-  //           headers: {
-  //           "Content-Type": "application/json; charset=UTF-8",
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //        }
-  //     );
-  //     console.log(response.data)
-  //     setUsersMessages(response.data)
-  //     console.log(usersMessages)
-  //  } catch (error) {
-      
-  //  }
-  // }
 
   const getInfoUser = async ()=>{
     try {
       const id = cookies.UserId;
       const token = cookies.Token;
-      console.log(allConversation)
-      // console.log(allConversation.length)
       let listSenderId = [];
       for( let userCounter = 0;userCounter<allConversation.length; userCounter++){
-        // for(let j = 0; j<allConversation.at(i).members.length; j++){
           if(allConversation.at(userCounter).members.at(0) != id){
             listSenderId.push(allConversation.at(userCounter).members.at(0));
-            // break;
           }
           else {
             listSenderId.push(allConversation.at(userCounter).members.at(1));
           }
       }
-      console.log("listId")
-      console.log(listSenderId)
       let list = [];
       for(let i = 0; i<listSenderId.length; i++){
         const response = await axios.get(
@@ -93,85 +67,102 @@ const token = cookies.Token;
           }
         );
         list.push(response.data);
-        // console.log(listOfContactDetail);
-        console.log(list)
       }
-      // console.log(list);
-      // setListOfContactDetail((listOfContactDetails) => [...listOfContactDetails, ...list]);
       setListOfContactDetail(list)
-      console.log(listOfContactDetails);
       
     } catch (error) {
-      
+      console.log(error)
     }
   }
 
+  const getMatchAccount = async (matchId) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/api/test/singleuser/id/${matchId}`,
+        {
+          headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const dataMatch = response.data;
+      setAccountData(dataMatch);
+      if(accountData.length>0 && accountData.userDetail){
+        setLoadingMatch(false)
+      }
+      else
+        setLoadingMatch(true)
+
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   useEffect(()=>{
     if(cookies){
-      // setConversation([])
-    getUserConversation()
-    getInfoUser()
+      setInterval(()=>{
+        getUserConversation()
+        getInfoUser()
+      }, 3000);
     }
-  }, [cookies])
+  }, [loading, matchId, accountData, loadingMatch])
 
   return (
-        <div class="w-full mx-auto">
-      <div class="border rounded lg:grid lg:grid-cols-4 w-full h-4/5">
-        <div class="border-r border-gray-300 lg:col-span-1 lg:block">
-          <div class="mx-3 my-3">
-            <div class="relative text-gray-600">
-              <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+        <div className="w-full mx-auto">
+      <div className="border rounded lg:grid lg:grid-cols-4 w-full h-4/5">
+        <div className="border-r border-gray-300 lg:col-span-1 lg:block">
+          <div className="mx-3 my-3">
+            <div className="relative text-gray-600">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  viewBox="0 0 24 24" class="w-6 h-6 text-gray-300">
+                  viewBox="0 0 24 24" className="w-6 h-6 text-gray-300">
                   <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                 </svg>
               </span>
-              <input type="search" class="block w-full py-2 pl-10 bg-gray-100 rounded outline-none" name="search"
+              <input type="search" className="block w-full py-2 pl-10 bg-gray-100 rounded outline-none" name="search"
                 placeholder="Search" required />
             </div>
           </div>
 
-          <ul class="overflow-auto h-[32rem]">
-            <h2 class="my-2 mb-2 ml-2 text-lg text-gray-600">Messages</h2>
+          <ul className="overflow-auto h-[32rem]">
+            <h2 className="my-2 mb-2 ml-2 text-lg text-gray-600">Messages</h2>
              <li>
-              {listOfContactDetails && listOfContactDetails.map(({_id, userDetail, first_name, last_name}) =>(
+              {listOfContactDetails.length>0 && allConversation.length>0 ? listOfContactDetails.map(({_id, userDetail, first_name, last_name}) =>(
               
                 <a key={_id} onClick={()=>setChat(_id)}
-                  class="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none">
-                      <img class="object-cover w-10 h-10 rounded-full"
+                  className="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none">
+                      <img className="object-cover w-10 h-10 rounded-full"
                     src={userDetail.profile_picture} alt="userpic" />
                   
-                  <div class="w-full pb-2">
-                     <div class="flex justify-between">
-                      <span class="block ml-2 font-semibold text-gray-600">{`${first_name} ${last_name}`}</span>
+                  <div className="w-full pb-2" onLoad={()=>setLoading(false)}>
+                     <div className="flex justify-between">
+                      <span className="block ml-2 font-semibold text-gray-600">{`${first_name} ${last_name}`}</span>
                     </div>
                   </div>
                 </a>
-              ))}
+              )) : <div>Fetching Messages...</div>}
               
             </li>
           </ul>
         </div>
-        <div class="hidden lg:col-span-2 lg:block">
-          <div class="w-full max-h-4/5">
+        <div className="hidden lg:col-span-2 lg:block">
+          <div className="w-full max-h-4/5">
             {clickedUser && <Chat
              matchId={matchId}
              >
               </Chat>}
           </div>
         </div>
-          {/* <div class="hidden lg:col-span-1 lg:block w-full">
+        <div class="hidden lg:col-span-1 lg:block w-full">
           <div class="h-full">
-            <div class="card w-full bg-base-100 shadow-xl h-full">
-              <figure><img src="https://api.lorem.space/image/face?w=150&h=150" alt="profile picture" className='w-full'/></figure>
-              <div class="card-body">
-                <h1 class="card-title">{`${accountData.first_name} ${accountData.last_name}Name`}</h1>
-                <h2 className='mb-[30px]'>{`${accountData.userDetail.occupation}test`}</h2>
-                <p>{`${accountData.userDetail.description}description`}</p>
-              </div>
-            </div>
+            {clickedUser && accountData && accountData.userDetail && 
+            <ChatProfile
+              accountData={accountData}
+            >
+            </ChatProfile>}
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   )
