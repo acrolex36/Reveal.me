680b46dd6bd35be89ef5e0acfb36863f21b02e2d
Dwiresti Puspita Rahmi <dwiresti.p.rahmi@stud.h-da.de>
2022-07-06 15:27:34 +0200

    [Frontend] trying to add function of enter button to send message

--- Frontend/reveal.me/src/Components/Chat/ChatContainer.jsx
+++ Frontend/reveal.me/src/Components/Chat/ChatContainer.jsx
@@ -175,6 +175,12 @@ const ChatContainer = () => {
 
   }
 
+  // const handleKeypress = e =>{
+  //   if(e.key === "Enter"){
+  //     sendMessage(e);
+  //   }
+  // }
+
   const getMessages = async (matchId) => {
     try {
       const res = await axios.get(
@@ -329,7 +335,7 @@ const ChatContainer = () => {
             
           <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
             <div className="relative flex">
-              <input type="text" value={textArea} placeholder="Write your message!" className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-3 bg-gray-200 rounded-md py-3 pr-36 max-h-28 box-border overflow-y-hidden scrollbar-thumb-blue scrolling-touch" onChange={(e)=>setTextArea(e.target.value)}/>
+              <input type="text" value={textArea} placeholder="Write your message!" className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-3 bg-gray-200 rounded-md py-3 pr-36 max-h-28 box-border overflow-y-hidden scrollbar-thumb-blue scrolling-touch" onChange={(e)=>setTextArea(e.target.value)} />
                 <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
                   <button type="button" className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-600">
