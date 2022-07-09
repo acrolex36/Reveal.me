c7f3ac2f24820670f1707150e12073f65277aa03
Dwiresti Puspita Rahmi <dwiresti.p.rahmi@stud.h-da.de>
2022-07-09 18:35:02 +0200

    [Frontend] cancel sending image as a message after clicking on image

--- Frontend/reveal.me/src/Components/Chat/ChatContainer.jsx
+++ Frontend/reveal.me/src/Components/Chat/ChatContainer.jsx
@@ -290,33 +290,10 @@ const ChatContainer = () => {
     <div className="w-full mx-auto">
       <div className="border rounded lg:grid lg:grid-cols-4 w-full h-4/5">
         <div className="border-r border-gray-300 lg:col-span-1 lg:block">
-          <div className="mx-3 my-3">
-            <div className="relative text-gray-600">
-              <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  viewBox="0 0 24 24"
-                  className="w-6 h-6 text-gray-300"
-                >
-                  <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                </svg>
-              </span>
-              <input
-                type="search"
-                className="block w-full py-2 pl-10 bg-gray-100 rounded outline-none"
-                name="search"
-                placeholder="Search"
-                required
-              />
-            </div>
-          </div>
-
           <ul className="overflow-auto h-[32rem]">
-            <h2 className="my-2 mb-2 ml-2 text-lg text-gray-600">Messages</h2>
+            <h2 className="mx-3 my-5 mb-2 ml-2 text-lg text-gray-600">
+              Messages
+            </h2>
             <li>
               {allConversation && allConversation.length > 0 ? (
                 allConversation.map((convo) => (
@@ -379,10 +356,11 @@ const ChatContainer = () => {
                           onChange={(e) => setTextArea(e.target.value)}
                         />
                       ) : (
-                        <img
-                          src={sendImage}
-                          className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-3 bg-gray-200 rounded-md py-3 pr-36 h-auto box-border overflow-y-hidden scrollbar-thumb-blue scrolling-touch"
-                        ></img>
+                          <img
+                            src={sendImage}
+                            className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-3 bg-gray-200 rounded-md py-3 pr-36 h-auto box-border overflow-y-hidden scrollbar-thumb-blue scrolling-touch"
+                            onClick={(e) =>setSendImage("")}
+                          ></img>
                       )}
                       <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
                         <label>
