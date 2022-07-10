e7e978da55c1470e6596ff2241476a7103aff4d4
Enrico Egen Selian <Enrico-Egen.Selian@external.telekom.de>
2022-07-10 12:08:38 +0200

    [Frontend] Change sidebar route from chat to messages

--- Frontend/reveal.me/src/Components/Sidebar.jsx
+++ Frontend/reveal.me/src/Components/Sidebar.jsx
@@ -94,7 +94,7 @@ const Sidebar = () => {
                     </li>
                     <li>
                         <a
-                            href="/chat"
+                            href="/messages"
                             className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
                         >
                             <svg
