d64802e7f15fc6b3c0394fe6b1b209eea8f2c579
Starmulj <Gotujato-46>
2022-07-10 20:39:29 +0200

    test Api done -divide controller into 3 -give comment to each route

--- Backend/reveal.me/routes/posts.ts
+++ Backend/reveal.me/routes/posts.ts
@@ -6,29 +6,37 @@ import {
 
   updateOneUser,
   updateOneUserProfile,
-  updateMatchedUser,
+  updateMatchedUserById,
+  removeMatchedUser,
+  updateSwipedLeftUsers,
+  removeOneSwipedLeftUsers,
   
   getAllUser,
   getOneUserDetailwithId,
-  getOneUserDetail,
-  getAllFilteredUser,
   getAllFilteredUserById,
-  updateMatchedUserById,
-  createConversation,
-  getAllConversation,
-  updateMessages,
-  getOneConversation,
-  getAllConversationFromOneUser,
-  getTotalMessage,
-  getOneConversationById,
-  updateMessage,
-  getTotalMessages,
-  getAllMessages,
-  deleteUser,
-  removeMatchedUser,
-  updateSwipedLeftUsers
 
-} from "../controllers/posts";
+  deleteUser,
+
+} from "../controllers/postsUser";
+
+import {
+  createConversation,
+
+  updateIsBlurred,
+
+  getAllConversation,
+  getAllConversationFromOneUser,
+  getTotalMessages,
+
+  deleteConversation,
+
+} from "../controllers/postsConversation"
+
+import {
+  getAllMessages,
+  createMessage,
+
+} from "../controllers/postsMessage"
 
 const router = express.Router();
 
@@ -39,32 +47,31 @@ router.post("/auth/login/forgetpassword", forgetpassword);
 
 router.put("/user/profile/head/:email", updateOneUser);
 router.put("/user/profile/body/:email", updateOneUserProfile);
-router.put("/user/profile/:email/:matchedUserEmail", updateMatchedUser); //not use
 router.put("/user/profile/id/:id/:matchedUserId", updateMatchedUserById);
 router.put("/user/profile/remove/id/:id/:matchedUserId", removeMatchedUser);
 router.put("/user/profile/swipedleft/id/:id/:matchedUserId", updateSwipedLeftUsers);
-
+router.put("/user/profile/swipedleft/remove/id/:id", removeOneSwipedLeftUsers);
 
 router.delete("/user/:id", deleteUser)
 
 router.get("/alluser", getAllUser);
 router.get("/singleuser/id/:id", getOneUserDetailwithId);
-router.get("/singleuser/email/:email", getOneUserDetail); //not use
 router.get("/filtereduser/id/:id", getAllFilteredUserById);
-router.get("/filtereduser/email/:email", getAllFilteredUser); //not use
 
-//Message
+//Conversation
 router.post("/conversation/message/:userId1/:userId2", createConversation);
-router.post("/message/:conversationId", updateMessage)
 
-router.put("/conversation/message/:id", updateMessages);//not use
+router.put("/conversation/isblurred/:conversationId", updateIsBlurred);
 
 router.get("/allconversation", getAllConversation);
 router.get("/allconversation/:userId", getAllConversationFromOneUser);
-router.get("/oneconversation/:userId1/:userId2", getOneConversation);//not use
-router.get("/oneconversationid/:id", getOneConversationById);//not use
-router.get("/oneconversation/totalmessage/:userId1/:userId2", getTotalMessage);//not use
 router.get("/message/total/:conversationId", getTotalMessages)
+
+router.delete("/conversation/remove/:conversationId", deleteConversation)
+
+//Message
+router.post("/message/:conversationId", createMessage)
+
 router.get("/message/all/:conversationId", getAllMessages)
 
 export default router;
